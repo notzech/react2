e4d8fb659d86e55d2507b76dce44042d81d44640
notzech <Zechariahjohnson21@gmail.com>
2020-03-10 14:04:51 -0400

    up3

--- src/App.js
+++ src/App.js
@@ -12,13 +12,7 @@ import newsPage from "./News";
 import classesBase from "./Classes/Classes";
 export default function Homepage() {
     return <Router>
-        <head>
-            <meta charSet={"utf-8"}/>
-            <meta name={"viewport"} content={"width=device-width, initial-scale=1.0"}/>
-            <meta http-equiv={"X-UA-Compatible"} content={"ie=edge"}/>
-            <link rel={"stylesheet"} href={"https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"} />
-            <title>Walker career center</title>
-        </head>
+
 
         <div>
             <article id={"topNav"}>
